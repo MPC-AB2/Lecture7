abf8dff3e86e690f7cb7ada144b596652ac2bc38
cobolaci <xziakm00@vutbr.cz>
2022-03-31 15:32:03 +0200

    cobolaci check overalped boxes

--- cobolaci/cobolaci.m
+++ cobolaci/cobolaci.m
@@ -1,22 +1,21 @@
 function [segmentedImages] = cobolaci(pathToImages)
-    detector = maskrcnn("resnet50-coco");    
+detector = maskrcnn("resnet50-coco");    
     cd(pathToImages);
     a = dir('*.jpg');
     n = numel(a);
-    5;
     segmentedImages = cell(1,n);
-    nonCat = 0; 
+    nonCat = 0;
     for i = 1:n
         nonCat = 0;
         I = imread(['cat' num2str(i) '.jpg']);
 %         cat = imresize(I,[800 1200]);
-        t = 0.75;
+        t = 0.55;
         [masks,labels,scores,boxes] = segmentObjects(detector,I,Threshold = t);
-        
+       
         f = find(labels == 'cat');
         while isempty(f) == 1
             t = t-0.5;
-            [masks,labels,scores,boxes] = segmentObjects(detector,I,Threshold = t);
+            [masks,labels,scores,boxes] = segmentObjects(detector,I,t);
             f = find(labels == 'cat');
                 if t == 0.05
                     nonCat = 1;
@@ -24,15 +23,31 @@ function [segmentedImages] = cobolaci(pathToImages)
                 end
         end
 
-        if nonCat == 1 && i >1;
+        if nonCat == 1 && i >1
             segmentedImages{i} = segmentedImages{i-1};
-        else nonCat == 1 && i == 1;
+        else nonCat == 1 && i == 1
             segmentedImages{i} = zeros(size(I,1),size(I,2));
         end
         segmentedIm = squeeze(sum(masks(:,:,f),3));
-        segmentedIm(segmentedIm >1) = 1;
+       
+        if sum(sum(segmentedIm > 1))
+            cats = length(f);
+   
+            overlap = bboxOverlapRatio(boxes(f(1),:),boxes(f(2),:));  
+   
+            if overlap > 0.15
+                if sum(sum(masks(:,:,f(2)))) < sum(sum(masks(:,:,f(1))))
+                    segmentedIm = masks(:,:,f(2));
+                else
+                    segmentedIm = masks(:,:,f(1));
+                end
+   
+            end
+        end
+       
         segmentedImages{i} = logical(segmentedIm);
 
-    end
+%         imshow(segmentedImages{i})
 
+    end
 end
