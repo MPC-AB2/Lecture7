2a2ef9cbae886d4216332a7fb1d22f3484131665
ASchneiderova <asackob@gmail.com>
2022-03-31 15:32:32 +0200

    lapsi?
    Signed-off-by: ASchneiderova <asackob@gmail.com>

--- kafickari/kafickari.m
+++ kafickari/kafickari.m
@@ -2,11 +2,11 @@ function [masks_merged] = kafickari(path_directory)
 %%
  % Pls note the format of files,change it as required
  original_files=dir([path_directory '/*.jpg']); 
-
+num = 300;
 
 detector = maskrcnn("resnet50-coco");
 
- obr = {};
+obr = {};
  masks = {};
  masks_new = {};
  labels = {};
@@ -15,10 +15,15 @@ detector = maskrcnn("resnet50-coco");
  for k=1:length(original_files)
     filename=[path_directory '\' original_files(k).name];
     obr{k}= imread(filename);
-    [masks{k},labels{k},scores{k},boxes{k}] = segmentObjects(detector,obr{k},Threshold=0.60);
+    obr{k} = padarray(obr{k},[300 300],0);
+    
+    [masks{k},labels{k},scores{k},boxes{k}] = segmentObjects(detector,obr{k},Threshold=0.55);
     tmp = labels{1,k};
-    tmp=cellstr(tmp);
+    tmp = cellstr(tmp);
     idx = find(strcmp(tmp,'cat' ));   
+    if isempty(idx)
+       idx = find(strcmp(tmp,'dog' ));   
+    end
     mask_tmp = [];
 
     for i=1:length(idx)
@@ -29,7 +34,6 @@ detector = maskrcnn("resnet50-coco");
 %     overlayedImage = insertObjectMask(obr{k},logical(mask_tmp));
 %     figure
 %     imshow(overlayedImage)
-
 
  end
 
@@ -42,10 +46,13 @@ for i = 1:length(masks_new)
     for k = 1:size(masks_new{i},3)
         mask(masks_new{i}(:,:,k)==1) = 1;
     end
+    mask = mask(301:end-300,301:end-300, :);
     masks_merged{i} = logical(mask);
 
 
 end
+
+
 
 
 end
