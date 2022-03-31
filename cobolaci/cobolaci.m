078eecc533244c46da11dc37b262f7c54d8c1ab9
cobolaci <xziakm00@vutbr.cz>
2022-03-31 13:19:01 +0200

    comoblaci v.01

---
+++ cobolaci/cobolaci.m
@@ -0,0 +1,38 @@
+function [segmentedImages] = cobolaci(pathToImages)
+    detector = maskrcnn("resnet50-coco");    
+    cd(pathToImages);
+    a = dir('*.jpg');
+    n = numel(a);
+    5;
+    segmentedImages = cell(1,n);
+    nonCat = 0; 
+    for i = 1:n
+        nonCat = 0;
+        I = imread(['cat' num2str(i) '.jpg']);
+%         cat = imresize(I,[800 1200]);
+        t = 0.75;
+        [masks,labels,scores,boxes] = segmentObjects(detector,I,Threshold = t);
+        
+        f = find(labels == 'cat');
+        while isempty(f) == 1
+            t = t-0.5;
+            [masks,labels,scores,boxes] = segmentObjects(detector,I,Threshold = t);
+            f = find(labels == 'cat');
+                if t == 0.05
+                    nonCat = 1;
+                    break
+                end
+        end
+
+        if nonCat == 1 && i >1;
+            segmentedImages{i} = segmentedImages{i-1};
+        else nonCat == 1 && i == 1;
+            segmentedImages{i} = zeros(size(I,1),size(I,2));
+        end
+        segmentedIm = squeeze(sum(masks(:,:,f),3));
+        segmentedIm(segmentedIm >1) = 1;
+        segmentedImages{i} = segmentedIm;
+
+    end
+
+end
