fc8f70e969667fc634f9fda05f6570bf48d1a778
ondrados <ondrejdoskocil@gmail.com>
2022-03-31 13:45:40 +0200

    prvni super update

--- prvni.m
+++ prvni.m
@@ -6,23 +6,47 @@ detector = maskrcnn("resnet50-coco");
 for i = 1:length(img_paths)
    
     I = imread([img_paths(i).folder, '\', img_paths(i).name]);
-    [masks,labels,scores,boxes] = segmentObjects(detector,I,Threshold=0.60);%, MinSize=[100 100], NumStrongestRegions=2);
+    [masks,labels,scores,boxes] = segmentObjects(detector,I,Threshold=0.6);%, MinSize=[100 100], NumStrongestRegions=2);
     
     my_labels = labels(labels=='cat');
     my_masks = masks(:,:,labels=='cat');
     my_scores = scores(labels=='cat');
+
     if length(my_labels)==1 
         mask = my_masks;
     
         maskStack = mask;
         maskStack(:,:,2) = mask;
-        mask = activecontour(I,mask,110, 'Chan-Vese', 'SmoothFactor', 1.5);
+        mask = activecontour(I,mask,100, 'Chan-Vese', 'SmoothFactor', 1.5);
         maskStack(:,:,1) = mask;
     
         numMasks = size(maskStack,3);
         overlayedImage = insertObjectMask(I,maskStack,'Color',lines(numMasks));
-    else   
-        mask_thresh = my_masks(:,:,my_scores>=0.92);
+    else
+        %Prekryv
+        indexy = [];
+        for u = 1:size(my_masks,3)
+            for y = 1:size(my_masks,3)
+                if u==y
+                    continue
+                end
+                if sum(sum(((my_masks(:,:,u)+my_masks(:,:,y))==2)))==0
+                    indexy = [indexy, u, y];                
+                end
+    
+            end
+        end
+
+        indexy = sort(indexy);
+        indexy = unique(indexy);
+
+        if length(indexy)~=0
+            mask_thresh = my_masks(:,:,indexy);
+        else
+            [~,idx] = max(my_scores);
+            mask_thresh = my_masks(:,:,idx);
+        end
+
         mask = sum(mask_thresh,3);
         mask(mask>1) = 1;
         mask = logical(mask);
@@ -33,10 +57,9 @@ for i = 1:length(img_paths)
     
         numMasks = size(maskStack,3);
         overlayedImage = insertObjectMask(I,maskStack,'Color',lines(numMasks));
-
     end
     segmentedImages{i} = mask;
 
-%     figure
-%     imshow(overlayedImage)
+    figure
+    imshow(overlayedImage)
 end
